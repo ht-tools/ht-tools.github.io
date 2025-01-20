319ab5c17f6363a3e590d81413ce80ecfe1841e8
ht-tools <small.year2891@fastmail.com>
2025-01-20 13:56:06 -0500

    First major update with correct format. Still no functionality.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 function paint(color) {
-    const circle = document.getElementById('circleID');
-    circle.style = `background-color:${color}`;
+    const circle1 = document.getElementById('circleFC1');
+    circle1.style = `background-color:${color}`;
+    const circle2 = document.getElementById('circleFC2');
+    circle2.style = `background-color:${color}`;
+    localStorage.setItem("savedColor", color);
   }
   
+function loadVariables() {
+    var savedColor = localStorage.getItem("savedColor");
+    if (savedColor) {
+        paint(savedColor);
+    }
+}
